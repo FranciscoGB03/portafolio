2c96e56162999c094f7d2fe499b393e37e11fb87
FranciscoGB03 <fgonzalezb@toluca.tecnm.mx>
2020-07-20 20:39:44 -0500

    Updates

---
+++ precache-manifest.326e736b35a78d001db6ab7b0edc50f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d3c59f0841bb6d584de264091c0ca0",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "189401e5d4ca17012599",
+    "url": "/portafolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea6d1bb458a9bb958748",
+    "url": "/portafolio/static/js/2.ba1df81f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portafolio/static/js/2.ba1df81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "189401e5d4ca17012599",
+    "url": "/portafolio/static/js/main.469258b6.chunk.js"
+  },
+  {
+    "revision": "8de80431b645f6e2f0b8",
+    "url": "/portafolio/static/js/runtime-main.d29b2e7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portafolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
