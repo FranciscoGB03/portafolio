60f4d67eaba65f613cb01d86e225fcd3a267b751
FranciscoGB03 <fgonzalezb@toluca.tecnm.mx>
2020-07-21 22:57:08 -0500

    Updates

---
+++ precache-manifest.75691439c465ddcee10ff0c55d0c2fc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be79f2d87370ea46d89fc4fac9b4bb85",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "0cf82aa26e322de89b8a",
+    "url": "/portafolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "01659e5e92398c5e4671",
+    "url": "/portafolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0cf82aa26e322de89b8a",
+    "url": "/portafolio/static/js/2.4237e9fb.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/portafolio/static/js/2.4237e9fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01659e5e92398c5e4671",
+    "url": "/portafolio/static/js/main.260dd3aa.chunk.js"
+  },
+  {
+    "revision": "d30650f1ec318e36fc57",
+    "url": "/portafolio/static/js/runtime-main.1d39578f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portafolio/precache-manifest.36333abd3681fec21ddac1b61ce17057.js"
+  "/portafolio/precache-manifest.75691439c465ddcee10ff0c55d0c2fc1.js"
 );
 
 self.addEventListener('message', (event) => {
