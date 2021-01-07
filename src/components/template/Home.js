574b46b320ece553945a653d60b013c8d848201a
FranciscoGB03 <fgonzalezb@toluca.tecnm.mx>
2021-01-07 12:07:09 -0600

    Componente de Home

--- src/components/template/Home.js
+++ src/components/template/Home.js
@@ -5,7 +5,8 @@ const Home =()=>{
     return(
         <Template>
 			<div className="text-center">
-				<h1>Bienvenido a mi portafolio creado con react</h1>				
+				<h1>Ingeniero en Tecnologías de la Información y Comunicaciones Francisco González Bobadilla</h1>				
+				<h3>(Título en proceso)</h3>
 			</div>
 		</Template>
     );
